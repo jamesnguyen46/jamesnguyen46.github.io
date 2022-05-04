85e3b01307875494d1ec6d5648363ee42f599319
jamesnguyen46 <thachnguyen1989@gmail.com>
2022-05-04 11:22:45 +0700

    Add new workflow to format, check lint code and test django project

--- resume/firebase.py
+++ resume/firebase.py
@@ -2,11 +2,11 @@ from os import environ as env
 import pyrebase
 
 config = {
-  "apiKey": env.get('FIREBASE_API_KEY'),
-  "authDomain": env.get('FIREBASE_AUTH_DOMAIN'),
-  "databaseURL": env.get('FIREBASE_DATABASE_URL'),
-  "storageBucket": env.get('FIREBASE_STORAGE_BUCKET')
+    "apiKey": env.get("FIREBASE_API_KEY"),
+    "authDomain": env.get("FIREBASE_AUTH_DOMAIN"),
+    "databaseURL": env.get("FIREBASE_DATABASE_URL"),
+    "storageBucket": env.get("FIREBASE_STORAGE_BUCKET"),
 }
-firebase=pyrebase.initialize_app(config)
+firebase = pyrebase.initialize_app(config)
 authe = firebase.auth()
-database=firebase.database()
+database = firebase.database()
