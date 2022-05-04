8a95d15ce7bb09e16ced40ccbb6037ba354bd284
James <thachnguyen1989@gmail.com>
2022-05-04 11:55:03 +0700

    Add new workflow (#5)
    - Pre-commit to format code, check lint code
    - Test django project

--- manage.py
+++ manage.py
@@ -3,10 +3,10 @@
 import os
 import sys
 
-
+# pylint: disable=C0415
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_web_app.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_web_app.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,5 +18,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
